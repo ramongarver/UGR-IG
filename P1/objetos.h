6a74ed5d71c26195c3215b43aca5ff1112495ee4
ramongarver <ramongarver2000@gmail.com>
2020-11-06 15:19:37 +0100

    Refactor and Finish P1

--- P1/objetos.h
+++ P1/objetos.h
@@ -1,7 +1,3 @@
-//**************************************************************************
-// Práctica 1 usando objetos
-//**************************************************************************
-
 #include <stdlib.h>
 #include <vector>
 #include <GL/gl.h>
@@ -11,55 +7,49 @@ using namespace std;
 
 const float AXIS_SIZE = 5000;
 
-//*************************************************************************
-// clase punto
-//*************************************************************************
-
+//*************
+// _puntos3D //
+//*************
 class _puntos3D
 {
 	public:
-		_puntos3D();
-		void 	draw_puntos(float r, float g, float b, int grosor);
-
 		vector<_vertex3f> vertices;
+
+		_puntos3D();
+		void draw_puntos(float r, float g, float b, int grosor);	
 };
 
 
-//*************************************************************************
-// clase triángulo
-//*************************************************************************
-
+//*****************
+// _triangulos3D //
+//*****************
 class _triangulos3D: public _puntos3D
 {
 	public:
-		_triangulos3D();
-		void 	draw_aristas(float r, float g, float b, int grosor);
-		void    draw_solido(float r, float g, float b);
-		void 	draw_solido_ajedrez(float r1, float g1, float b1, float r2, float g2, float b2);
-
 		vector<_vertex3i> caras;
+
+		_triangulos3D();
+		void draw_aristas(float r, float g, float b, int grosor);
+		void draw_solido(float r, float g, float b);
+		void draw_solido_ajedrez(float r1, float g1, float b1, float r2, float g2, float b2);
 };
 
 
-//*************************************************************************
-// clase cubo
-//*************************************************************************
-
+//*********
+// _cubo //
+//*********
 class _cubo: public _triangulos3D
 {
 	public:
-		_cubo(float tam=0.5);
+		_cubo(float tam = 0.5);
 };
 
 
-//*************************************************************************
-// clase piramide
-//*************************************************************************
-
+//*************
+// _piramide //
+//*************
 class _piramide: public _triangulos3D
 {
 	public:
-		_piramide(float tam=0.5, float al=0.75);
+		_piramide(float tam = 0.5, float al = 0.75);
 };
-
-
